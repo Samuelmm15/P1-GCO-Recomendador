c35d219b0e9da8035445f819aa8ef475e6993c14
Samuel Martín <alu0101359526@ull.edu.es>
2023-10-16 17:28:58 +0100

    Fixed the implementation of the cosine distance calculate for the similarity matrix

--- recommendation-system/metrics/cosine_distance.py
+++ recommendation-system/metrics/cosine_distance.py
@@ -4,26 +4,16 @@
 # Description: This file contains the implementation of the function that calculates the cosine distance.
 
 import numpy as np
-
-def cosine_distance_similarity(row1, row2):
-  dot_product = np.dot(row1, row2)
-  norm_row1 = np.linalg.norm(row1) # Norma euclídea, se refiere a la extensión de este vector en el espacio.
-  norm_row2 = np.linalg.norm(row2)
-  return dot_product / (norm_row1 * norm_row2)
+# Importamos la librería que nos permite calcular la similitud coseno.
+from sklearn.metrics.pairwise import cosine_similarity
 
 def cosine_distance(utility_matrix):
-  # Se obtienen las dimensiones de la matriz de utilidad.
-  number_of_elements, number_of_users = utility_matrix.shape
+  # Manejar los valores NaN llenándolos con ceros, ya que la librería no permite el uso de nan.
+  utility_matrix = np.nan_to_num(utility_matrix, nan=0)
+
+  # Se realiza el cáclulo de la similitud haciendo uso de la distancia coseno.
+  similarity_matrix = cosine_similarity(utility_matrix)
   
-  # Inicializamos la matriz de similitud con ceros.
-  similarity_matrix = np.zeros((number_of_elements, number_of_elements))
-  
-  # A partir de este punto se realiza el cálculo de la matriz de similitud.
-  for i in range(number_of_elements):
-    for j in range(number_of_elements):
-      similitarity = cosine_distance_similarity(utility_matrix[i], utility_matrix[j])
-      similarity_matrix[i, j] = similitarity
-      similarity_matrix[j, i] = similitarity # Debido a que la matriz es simétrica.
-      
   # Se comprueba como resulta la matriz de similitud.
   print(similarity_matrix)
+
